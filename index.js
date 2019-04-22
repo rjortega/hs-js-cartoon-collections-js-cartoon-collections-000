bac1583504f34d0d92dff2965492c9d9a5c65844
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-22 18:27:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,24 +7,48 @@ function dwarfRollCall(dwarves) {
   return listArr
 }
 
+// function dwarfRollCall(dwarves) {
+// 	var endString = ""
+//   for (var i = 0; i < dwarves.length; i++) {
+//    endString += (i+1).toString()+ ". " + dwarves[i] + " ";
+//   };
+//   return endString;
+// }
+
 function summonCaptainPlanet(planeteerCalls){
   var arr = []
   for (let i = 0; i < planeteerCalls.length; i++){
-  arr.push(`${planeteerCalls[i].toUpperCase()}!`)
+    arr.push(`${planeteerCalls[i].toUpperCase()}!`)
   } return arr
 }
 
+// function summonCaptainPlanet(planeteerCalls){
+// 	var calls = [];
+//   for (var i = 0; i < planeteerCalls.length; i++) {
+//    calls.push(planeteerCalls[i] + "!");
+//   };
+//   return calls;
+// }
+
 function longPlaneteerCalls(words) {
-  var wordsLength = []
   for (let i = 0; i < words.length; i++){
-    wordsLength.push(words[i].length)
-    if (wordsLength[i] > 4){
+    if (words[i].length > 4){
       return true
     } else {
       return false
     }
   }
 }
+
+// function longPlaneteerCalls(words) {
+// 	var hasLongWords = false;
+//   for (var i = 0; i < words.length; i++) {
+//     if (words[i].length > 4){
+//       hasLongWords = true;
+//     }
+//   };
+//   return hasLongWords;
+// }
 
 function findTheCheese (foods) {
   for (let i = 0; i < foods.length; i++){
@@ -38,3 +62,15 @@ function findTheCheese (foods) {
   }
   return "no cheese!"
 }
+
+// function findTheCheese (foods) {
+// 	var cheeses = ["cheddar", "gouda", "camembert"];
+// 	for (var i = 0; i < foods.length; i++) {
+// 		for (var j = 0; j < cheeses.length; j++) {
+// 			if ( cheeses[j]=== foods[i] ) {
+// 				return foods[i]; //will quit and return the first cheese that it finds
+// 			}
+// 		}
+// 	}
+// 	return "no cheese!"; // returns "no cheese!" if it doesn't find any cheese :(
+// }
